25173370371a522388575567e5a083174b9552d6
Gaurav Gautam <gaurav@goldcast.io>
2021-10-16 12:25:29 +0530

    Cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,14 @@
-// import { App, paint } from "./pilcrow";
 import { newApp } from "./buttonwin.pil";
 
-// console.log("imported: ", newApp);
 newApp.onButtonPress = () => {
   newApp.setState("pressed");
-  // App.item.state = "pressed";
 }
 
 newApp.onButtonRelease = () => {
   newApp.setState("normal");
-  // App.item.state = "normal";
 }
 
 const canvas = document.getElementById("app") as HTMLCanvasElement;
 newApp.mount(canvas).then(() => {
   newApp.paint();
 });
-
-// import parser from "./parser";
-// console.log(
-// parser.parse(`Item { id: myButton_Symbol }`)
-// );
---
+++ src/pilcrow.js
@@ -0,0 +1,79 @@
+export const newApp = {
+  item: {},
+  mount(canvas) {
+    this.canvas = canvas;
+    const context = canvas.getContext("2d");
+    if (context) {
+      this.context = context;
+    }
+
+    this.item.images = this.item.images.map(image => {
+      let img = new Image();
+      img.src = image.source;
+      return {
+        ...image,
+        ref: img,
+        downloaded: new Promise((resolve, reject) => {
+          image.onload = resolve;
+          image.onerror = reject;
+          image.onabort = reject;
+        })
+      }
+    });
+
+    if (this.item.mouseArea && this.item.mouseArea.mousedown) {
+      canvas.addEventListener("mousedown", (ev) => {
+        if (ev.x <= this.item.mouseArea.width && ev.y <= this.item.mouseArea.height && this.onButtonPress) {
+          this.onButtonPress();
+        }
+      });
+    }
+
+    if (this.item.mouseArea && this.item.mouseArea.mouseup) {
+      canvas.addEventListener("mouseup", (ev) => {
+        if (ev.x <= this.item.mouseArea.width && ev.y <= this.item.mouseArea.height && this.onButtonRelease) {
+          this.onButtonRelease();
+        }
+      });
+    }
+
+    this.setState(null, true);
+    
+    return Promise.all(this.item.states.map(it => it.downloaded)).then(() => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve();
+        }, 100);
+      });
+    });
+  },
+  setState(state, nopaint) {
+    state = state || this.item.state;
+    const stateConfig = this.item.states.find(stateConf => stateConf.name === state);
+    for (let change of stateConfig.propertyChanges) {
+      const targetImg = this.item.images.find(image => image.id === change.target);
+      if (targetImg) {
+        targetImg.visible = change.visible;
+      }
+    }
+    if (!nopaint) {
+      this.paint();
+    } 
+  },
+  paint() {
+    const item = this.item;
+    const context = this.context;
+    const images = item.images.filter(image => image.visible);
+
+    for (let image of images) {
+      context.drawImage(image.ref, image.x, image.y, item.width, item.height);
+    }
+
+    let mouseArea = this.item.mouseArea;
+    if (mouseArea && mouseArea.draw) {
+      context.strokeStyle = "#FF0000";
+      context.rect(mouseArea.x, mouseArea.y, mouseArea.width, mouseArea.height);
+      context.stroke();
+    }
+  }
+};
